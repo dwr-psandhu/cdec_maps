c961437411fdd5293beb310df54d4f89bcad7754
Nicky Sandhu <psandhu@water.ca.gov>
2021-11-02 16:16:53 -0700

    switched to decorator module usage

--- cdec_maps/cdec_cache.py
+++ cdec_maps/cdec_cache.py
@@ -5,9 +5,10 @@ import functools
 import os
 import glob
 import pandas as pd
+from decorator import decorator
 
-
-def cache_to_file(list=False, expires='1D', cache_dir='cdec_cache', data=True):
+@decorator
+def cache_to_file(func, list=False, expires='1D', cache_dir='cdec_cache', *args, **kwargs):
     """Caches Dataframe returned by function to filename (name of function + '.pkl')
 
     Cache expires after "expires" Timedelta at which point the function is called and refreshes the cache before returning.
@@ -15,7 +16,6 @@ def cache_to_file(list=False, expires='1D', cache_dir='cdec_cache', data=True):
         list (boolean, optional): Indicates that the function returns a list (or other iterable) of DataFrame(s)
         expires (str, optional): [Cache expires after the Timedelta str]. Defaults to '1D'
         cache_dir (str, optional): Directory in which to store the pickled dataframe.
-        data (boolean, optional): Indicates that the data frames are appendable and can be updated with additional data rather than replacement
     """
     def expired(mtime):
         return pd.Timestamp.now() - pd.Timestamp.fromtimestamp(mtime) > pd.to_timedelta(expires)
@@ -24,67 +24,61 @@ def cache_to_file(list=False, expires='1D', cache_dir='cdec_cache', data=True):
         if not os.path.exists(cache_dir):
             os.mkdir(cache_dir)
 
-    def decorator(func):
+    def needs_creation(fname):
+        return not os.path.exists(fname)
 
-        @functools.wraps(func)
-        def wrapper_decorator(*args, **kwargs):
-            def needs_creation(fname):
-                return not os.path.exists(fname)
+    def needs_updating(fname):
+        return expired(os.path.getmtime(fname))
 
-            def needs_updating(fname):
-                return expired(os.path.getmtime(fname))
+    def needs_refresh(fname):
+        return needs_creation(fname) or needs_updating(fname)
+    #
 
-            def needs_refresh(fname):
-                return needs_creation(fname) or needs_updating(fname)
-            #
+    def read_cache(fname):
+        df = pd.read_pickle(fname)
+        return df
+    #
 
-            def read_cache(fname):
-                df = pd.read_pickle(fname)
-                return df
-            #
-
-            def write_cache(df, fname):
-                df.to_pickle(fname)
-            #
-            ensure_dir(cache_dir)
-            if list:
-                cached_files = glob.glob(f"{cache_dir}/{func.__name__}.{args[1]}.*.pkl")
-                if not cached_files or any([needs_refresh(fname) for fname in cached_files]):
-                    result = func(*args, **kwargs)
-                    for i, r in enumerate(result):
-                        write_cache(r, f"{cache_dir}/{func.__name__}.{args[1]}.{i}.pkl")
-                else:
-                    result = [read_cache(fname) for fname in cached_files]
+    def write_cache(df, fname):
+        df.to_pickle(fname)
+    #
+    ensure_dir(cache_dir)
+    if list:
+        cached_files = glob.glob(f"{cache_dir}/{func.__name__}.{args[1]}.*.pkl")
+        if not cached_files or any([needs_refresh(fname) for fname in cached_files]):
+            result = func(*args, **kwargs)
+            for i, r in enumerate(result):
+                write_cache(r, f"{cache_dir}/{func.__name__}.{args[1]}.{i}.pkl")
+        else:
+            result = [read_cache(fname) for fname in cached_files]
+    else:
+        if func.__name__ == 'read_station_data':
+            cache_file = f"{cache_dir}/{args[1]}_{args[2]}_{args[3]}.pkl"
+        else:
+            cache_file = f"{cache_dir}/{func.__name__}.pkl"
+        if needs_creation(cache_file):
+            if func.__name__ == 'read_station_data':  # then cache and fetch differently
+                # station_id, sensor_number, duration_code
+                result = args[0].read_entire_station_data_for(args[1], args[2], args[3])
             else:
-                if func.__name__ == 'read_station_data':
-                    cache_file = f"{cache_dir}/{args[1]}_{args[2]}_{args[3]}.pkl"
+                result = func(*args, **kwargs)
+            write_cache(result, cache_file)
+        elif needs_updating(cache_file):
+            if func.__name__ == 'read_station_data':  # need update strategy here
+                result = read_cache(cache_file)
+                if result.empty: # edge case if cache file has no data :(
+                    result = args[0].read_entire_station_data_for(args[1], args[2], args[3])
                 else:
-                    cache_file = f"{cache_dir}/{func.__name__}.pkl"
-                if needs_creation(cache_file):
-                    if func.__name__ == 'read_station_data':  # then cache and fetch differently
-                        # station_id, sensor_number, duration_code
-                        result = args[0].read_entire_station_data_for(args[1], args[2], args[3])
-                    else:
-                        result = func(*args, **kwargs)
-                    write_cache(result, cache_file)
-                elif needs_updating(cache_file):
-                    if func.__name__ == 'read_station_data':  # need update strategy here
-                        result = read_cache(cache_file)
-                        if result.empty: # edge case if cache file has no data :(
-                            result = args[0].read_entire_station_data_for(args[1], args[2], args[3])
-                        else:
-                            sdate = result.index[-1].strftime('%Y-%m-%d')
-                            dflatest = args[0]._undecorated_read_station_data(
-                                args[1], args[2], args[3], sdate, '')
-                            result = dflatest.combine_first(result)  # updates with latest fetched
-                    else:
-                        result = func(*args, **kwargs)
-                    write_cache(result, cache_file)
-                else:
-                    result = read_cache(cache_file)
-                if func.__name__ == 'read_station_data':  # then subset to desired time window
-                    start, end = args[0]._sort_times(args[4], args[5])
-                    result = result.loc[pd.to_datetime(start):pd.to_datetime(end)]
-            return result
-        return wrapper_decorator
-    return decorator
+                    sdate = result.index[-1].strftime('%Y-%m-%d')
+                    dflatest = args[0]._undecorated_read_station_data(
+                        args[1], args[2], args[3], sdate, '')
+                    result = dflatest.combine_first(result)  # updates with latest fetched
+            else:
+                result = func(*args, **kwargs)
+            write_cache(result, cache_file)
+        else:
+            result = read_cache(cache_file)
+        if func.__name__ == 'read_station_data':  # then subset to desired time window
+            start, end = args[0]._sort_times(args[4], args[5])
+            result = result.loc[pd.to_datetime(start):pd.to_datetime(end)]
+    return result
--- tests/test_cdec_data_read.py
+++ tests/test_cdec_data_read.py
@@ -28,3 +28,14 @@ def test_start_end_order():
     assert len(df) == 745
     assert df['VALUE'].iloc[0] == pytest.approx(104.31)
     assert df['VALUE'].index[0] == pd.Timestamp('2020-01-01')
+
+
+def test_read_station_data_kwargs():
+    '''Test by calling positional args with keywords syntax'''
+    c = cdec.Reader()
+    df = c.read_station_data(station_id='FPT', duration_code='H',
+                             sensor_number='1', end='2020-01-01', start='2020-02-01')
+    assert not df.empty
+    assert len(df) == 745
+    assert df['VALUE'].iloc[0] == pytest.approx(104.31)
+    assert df['VALUE'].index[0] == pd.Timestamp('2020-01-01')
