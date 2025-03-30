e933da4220bff6fc8d8ec258dd18caed38e0a248
Nicky Sandhu <psandhu@water.ca.gov>
2025-03-30 13:27:51 -0700

    test: Clear cache before running tests and standardize string formatting

--- tests/test_cdec_read_urls.py
+++ tests/test_cdec_read_urls.py
@@ -1,8 +1,10 @@
+from cdec_maps import cdec
 
-from cdec_maps import cdec, cdec_cache
 
 def test_read_all():
-    c=cdec.Reader()
+    cdec.cache.clear()
+    c = cdec.Reader()
+
     df = c.read_daily_stations()
 
     assert not df.empty
@@ -12,11 +14,11 @@ def test_read_all():
     assert not df.empty
     assert len(df) > 10
 
-    df=c.read_sensor_list()
+    df = c.read_sensor_list()
     assert not df.empty
     assert len(df) > 10
 
-    tlist = c.read_station_meta_info('FPT')
+    tlist = c.read_station_meta_info("FPT")
     df = tlist[0]
     assert not df.empty
     assert len(df) > 5
@@ -28,38 +30,44 @@ def test_read_all():
     assert not df.empty
     assert len(df) > 5
 
-    df = c.read_station_data('FPT',20,'D','2021-10-01','2021-10-21')
+    df = c.read_station_data("FPT", 20, "D", "2021-10-01", "2021-10-21")
     assert not df.empty
     assert len(df) > 10
 
-def test_lis(): # test for stations with 4 tables in station meta info, including a datum table
-    c=cdec.Reader()
-    dflist = c.read_station_meta_info('LIS')
+
+def test_lis():  # test for stations with 4 tables in station meta info, including a datum table
+    c = cdec.Reader()
+    dflist = c.read_station_meta_info("LIS")
     assert len(dflist) == 4
 
-def test_mck(): # test for MCK failed as no comments table but has a datum table
+
+def test_mck():  # test for MCK failed as no comments table but has a datum table
     c = cdec.Reader()
-    dflist = c.read_station_meta_info('MCK')
+    dflist = c.read_station_meta_info("MCK")
     assert len(dflist) == 4
+
 
 def test_change_cache_dir():
-    c=cdec.Reader()
+    c = cdec.Reader()
     df = c.read_daily_stations()
     assert not df.empty
     assert len(df) > 10
 
+
 def test_undefined_station():
-    c=cdec.Reader()
-    dflist = c.read_station_meta_info('BAW')
+    c = cdec.Reader()
+    dflist = c.read_station_meta_info("BAW")
     assert len(dflist) == 4
     assert dflist[0].empty
 
+
 def test_station_no_sensors():
-    c=cdec.Reader()
-    dflist = c.read_station_meta_info('NSW')
+    c = cdec.Reader()
+    dflist = c.read_station_meta_info("NSW")
     assert len(dflist) == 4
     assert dflist[1].empty
 
+
 def test_station():
-    c=cdec.Reader()
-    dflist = c.read_station_meta_info('LSV')
+    c = cdec.Reader()
+    dflist = c.read_station_meta_info("LSV")
